1a267590db43764cdc7de05a3aff89ac8192c3cc
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-09-05 15:10:24 -0300

    correções no exercicio

--- exercicios/media-notas/ex001.c
+++ exercicios/media-notas/ex001.c
@@ -2,22 +2,22 @@
 
 int main (){
 
-    int nota1, nota2, nota3;
+    float nota1, nota2, nota3;
     
 
     printf("Digite o valor das notas! \n ");
 
     printf("Primeira Nota: ");
-    scanf("%d \n",&nota1);
+    scanf("%f",&nota1);
 
     printf("Segunda Nota: ");
-    scanf("%d \n", &nota2);
+    scanf("%f", &nota2);
 
     printf("Terceira Nota: ");
-    scanf("%d \n", &nota3);
+    scanf("%f", &nota3);
 
-    int soma = nota1 + nota2 + nota3;
+    float soma = nota1 + nota2 + nota3;
     float media = soma / 3;
 
-    printf(" Media: %2f", media);
+    printf(" Media: %.2f", media);
 }
