b668cdbf08233d30b59e3d78b22155b32f107727
Tiago de Paula <168283080+estebantiago@users.noreply.github.com>
2025-09-25 10:57:40 -0300

    primeiro commit

---
+++ exercicios/022-ex/ex.c
@@ -0,0 +1,24 @@
+/*
+Este programa le o salario bruto de uma pessoa, a taxa de imposto de renda e a taxa de inss. Em seguida, mostra o seguinte menu
+
+ Escolha uma das opcoes abaixo: 
+  
+  1: Salario Liquido
+  2: Valor descontado pelo imposto de renda
+  3: Valor descontado pelo Inss.
+  
+  O usuarui seleciona a opcao e o programa exibe o que o usuario deseja.
+  
+  Caso nenhuma das opcoes seja escolhida, imprime-se mensagem de erro.
+  
+  */
+
+#include <stdio.h>
+
+int main (void){
+    printf("Escolha uma das opcoes abaixo: \n");
+
+    printf("1 - Salario liquido \n");
+    printf("2 - Valor descontado pelo imposto de renda. \n");
+    printf("3 - Valor descontado pelo Inss.");
+}
